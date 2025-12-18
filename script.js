b4f37e0787f1a0425230b3be67abdc9f1c7726cf
TDrozdz-dev <tomaszdrozdz.scuola@gmail.com>
2025-12-18 10:41:42 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 console.log("Inizio esecuzione");
+let d = new Date();
+console.log(d);
+document,getElementById("saluto").innerHTML=
+   d.getDay();
+
+
+
 function elabora(){
    let numero = document.getElementById(elemntid,"inNumeri").value;
    let output= document.getElementById("output");
    
        if (numero >= 0 && numero <= 255) {
-       output.innerText = "Numero valido: " + numero;
+
+          colori.push(numero);
+       
+          output.innerText = "Numero valido: " + numero;
+           
    } else {
         output.innerText ="Numero non valido. Inserisci un valore tra 0 e 255.";
        
@@ -15,3 +26,4 @@ function elabora(){
   function reset(){
       document.getElementById("inNumber").value = "";
   }
+
